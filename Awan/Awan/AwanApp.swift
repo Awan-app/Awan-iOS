30da17e8447ab2880529392732a082d868556992
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2026-07-15 13:57:02 +0300

    setup Awan project

---
+++ Awan/Awan/AwanApp.swift
@@ -0,0 +1,32 @@
+//
+//  AwanApp.swift
+//  Awan
+//
+//  Created by Me3bed on 15/07/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AwanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Awan/Awan/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Awan
+//
+//  Created by Me3bed on 15/07/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
