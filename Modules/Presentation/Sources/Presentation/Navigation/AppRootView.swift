e7b7c6e2f5a7ae9ce64c28b95c045baef4931dec
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2026-08-08 19:23:41 +0300

    AWAN-133: implement Google Sign-In logic in LoginViewModel and handle redirect in AppRootView

--- Modules/Presentation/Sources/Presentation/Navigation/AppRootView.swift
+++ Modules/Presentation/Sources/Presentation/Navigation/AppRootView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Common
+import GoogleSignIn
 
 struct AppRootView: View {
     private static let compactCreationDetent = PresentationDetent.height(370)
@@ -69,6 +70,9 @@ struct AppRootView: View {
             if status == .unauthenticated {
                 coordinator.authCoordinator.popToRoot()
             }
+        }
+        .onOpenURL { url in
+            GIDSignIn.sharedInstance.handle(url)
         }
     }
 
