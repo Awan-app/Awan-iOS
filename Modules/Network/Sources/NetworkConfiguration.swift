56bbb37cd31c20cecd54369c1fe42d4fc289a1cf
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2026-08-18 17:39:47 +0300

    update the base url

--- Modules/Network/Sources/NetworkConfiguration.swift
+++ Modules/Network/Sources/NetworkConfiguration.swift
@@ -1,5 +1,5 @@
 public enum NetworkConfiguration {
-    public static let backendBaseURL = "https://backend-production-c701.up.railway.app"
+    public static let backendBaseURL = "https://backend-production-369f.up.railway.app"
     public static let apiBaseURL = backendBaseURL + "/api/v1"
     public static let authBaseURL = apiBaseURL + "/auth"
 }
